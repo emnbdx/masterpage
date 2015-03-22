3779765f359a9d8ec30e9fb3c5838015e39b17ff
Eddy MONTUS <eddy.montus@gmail.com>
2015-03-22 21:01:39 +0100

    add analytics code

--- include/masterpage.php
+++ include/masterpage.php
@@ -147,17 +147,14 @@ class MasterPage {
 			echo '});';
 			echo '</script>';
 			
-			echo '<script type="text/javascript">';
-			echo "	var _gaq = _gaq || [];";
-			echo "	_gaq.push(['_setAccount', 'UA-39477605-1']);";
-			echo "	_gaq.push(['_trackPageview']);";
-			
-			echo "	(function() {";
-			echo "		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;";
-			echo "		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';";
-			echo "		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);";
-			echo "	})();";
-			echo "</script>";
+			echo '<script>';
+		  	echo '	(function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){';
+		  	echo '	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),';
+		  	echo '	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)';
+		  	echo '	})(window,document,\'script\',\'//www.google-analytics.com/analytics.js\',\'ga\');';
+		  	echo '	ga(\'create\', \'UA-40535132-1\', \'auto\');';
+		  	echo '	ga(\'send\', \'pageview\');';
+			echo '</script>';
 			
 			echo $this->_head;
 		echo '</head>';
